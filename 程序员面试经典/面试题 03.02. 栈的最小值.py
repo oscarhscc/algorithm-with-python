ec78b8119e70e7abe464b1768caed7554e7a95c3
Oscar Chen <cchen237-c@my.cityu.edu.hk>
2020-02-27 08:46:51 +0800

    Add files via upload

---
+++ 程序员面试经典/面试题 03.02. 栈的最小值.py
@@ -0,0 +1,73 @@
+'''
+请设计一个栈，除了常规栈支持的pop与push函数以外，还支持min函数，该函数返回栈元素中的最小值。执行push、pop和min操作的时间复杂度必须为O(1)。
+
+示例：
+MinStack minStack = new MinStack();
+minStack.push(-2);
+minStack.push(0);
+minStack.push(-3);
+minStack.getMin();   --> 返回 -3.
+minStack.pop();
+minStack.top();      --> 返回 0.
+minStack.getMin();   --> 返回 -2.
+
+'''
+
+class MinStack(object):
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.stack = []
+        self.min_stack = []
+        
+
+    def push(self, x):
+        """
+        :type x: int
+        :rtype: None
+        """
+        self.stack.append(x)
+        if not self.min_stack:
+            self.min_stack.append(x)
+        elif x <= self.min_stack[-1]:
+            self.min_stack.append(x)
+
+
+    def pop(self):
+        """
+        :rtype: None
+        """
+        x = self.stack.pop()
+        if x == self.min_stack[-1]:
+            self.min_stack.pop()
+
+
+    def top(self):
+        """
+        :rtype: int
+        """
+        if self.stack:
+            return self.stack[-1]
+        else:
+            return None
+
+
+    def getMin(self):
+        """
+        :rtype: int
+        """
+        if self.min_stack:
+            return self.min_stack[-1]
+        else:
+            return None
+
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(x)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
