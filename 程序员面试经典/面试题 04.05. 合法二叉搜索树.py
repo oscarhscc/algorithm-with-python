3af0845f1a27e7be03a6f847414d3a4555977c86
Oscar Chen <cchen237-c@my.cityu.edu.hk>
2020-02-29 09:40:10 +0800

    Add files via upload

---
+++ 程序员面试经典/面试题 04.05. 合法二叉搜索树.py
@@ -0,0 +1,43 @@
+'''
+实现一个函数，检查一棵二叉树是否为二叉搜索树。
+
+示例 1:
+输入:
+    2
+   / \
+  1   3
+输出: true
+示例 2:
+输入:
+    5
+   / \
+  1   4
+     / \
+    3   6
+输出: false
+解释: 输入为: [5,1,4,null,null,3,6]。
+     根节点的值为 5 ，但是其右子节点值为 4 。
+'''
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def isValidBST(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        res = []
+        def inorder(root):
+            if not root:
+                return None
+            inorder(root.left)
+            res.append(root.val)
+            inorder(root.right)
+        inorder(root)
+        return res == sorted(list(set(res)))
