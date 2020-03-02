53925682c83076722c554a9e669a23369867385d
Oscar Chen <cchen237-c@my.cityu.edu.hk>
2020-03-02 11:06:12 +0800

    Add files via upload

---
+++ 程序员面试经典/ 面试题 04.12. 求和路径.py
@@ -0,0 +1,42 @@
+'''
+给定一棵二叉树，其中每个节点都含有一个整数数值(该值或正或负)。设计一个算法，打印节点数值总和等于某个给定值的所有路径的数量。注意，路径不一定非得从二叉树的根节点或叶节点开始或结束，但是其方向必须向下(只能从父节点指向子节点方向)。
+
+示例:
+给定如下二叉树，以及目标和 sum = 22，
+
+              5
+             / \
+            4   8
+           /   / \
+          11  13  4
+         /  \    / \
+        7    2  5   1
+返回:
+
+3
+解释：和为 22 的路径有：[5,4,11,2], [5,8,4,5], [4,11,7]
+提示：
+
+节点总数 <= 10000
+'''
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def pathSum(self, root, sum):
+        """
+        :type root: TreeNode
+        :type sum: int
+        :rtype: int
+        """
+        def f(root, path):
+            if root:
+                path = [i + root.val for i in path] + [root.val]
+                return path.count(sum) + f(root.left, path) + f(root.right, path)
+            return 0
+        return f(root, []) 
