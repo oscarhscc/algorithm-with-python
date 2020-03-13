5a90a337ff8f9d89859316d092c5949ea18dd05e
Oscar Chen <cchen237-c@my.cityu.edu.hk>
2020-03-13 10:12:16 +0800

    Add files via upload

---
+++ 程序员面试经典/面试题 10.09. 排序矩阵查找.py
@@ -0,0 +1,42 @@
+'''
+给定M×N矩阵，每一行、每一列都按升序排列，请编写代码找出某元素。
+
+示例:
+
+现有矩阵 matrix 如下：
+
+[
+  [1,   4,  7, 11, 15],
+  [2,   5,  8, 12, 19],
+  [3,   6,  9, 16, 22],
+  [10, 13, 14, 17, 24],
+  [18, 21, 23, 26, 30]
+]
+给定 target = 5，返回 true。
+
+给定 target = 20，返回 false。
+
+通过次数601提交次数1,230
+'''
+
+class Solution(object):
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        if not matrix:
+            return False
+        width = len(matrix[0])
+        height = len(matrix)
+        i = 0
+        j = width - 1
+        while i < height and j >= 0:
+            if matrix[i][j] > target:
+                j -= 1
+            elif matrix[i][j] < target:
+                i += 1
+            else:
+                return True
+        return False
