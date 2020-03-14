73972b86ebe49aaa7f09f3547b78eb9e7789804a
Oscar Chen <cchen237-c@my.cityu.edu.hk>
2020-03-14 10:01:15 +0800

    Add files via upload

---
+++ 程序员面试经典/面试题 10.11. 峰与谷.py
@@ -0,0 +1,28 @@
+'''
+在一个整数数组中，“峰”是大于或等于相邻整数的元素，相应地，“谷”是小于或等于相邻整数的元素。例如，在数组{5, 8, 6, 2, 3, 4, 6}中，{8, 6}是峰， {5, 2}是谷。现在给定一个整数数组，将该数组按峰与谷的交替顺序排序。
+
+示例:
+
+输入: [5, 3, 1, 2, 3]
+输出: [5, 1, 3, 2, 3]
+提示：
+
+nums.length <= 10000
+'''
+
+class Solution(object):
+    def wiggleSort(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        if len(nums) < 3:
+            return nums
+        nums.sort()
+        right = len(nums) // 2
+        left = 0
+        while left < len(nums):
+            nums[left], nums[right] = nums[right], nums[left]
+            right += 1
+            left += 2
+        return nums
