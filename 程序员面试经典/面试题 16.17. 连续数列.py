6daac5fd4caf8e9fb0d580b491798318e2d7f410
Oscar Chen <cchen237-c@my.cityu.edu.hk>
2020-03-25 10:35:13 +0800

    Add files via upload

---
+++ 程序员面试经典/面试题 16.17. 连续数列.py
@@ -0,0 +1,26 @@
+'''
+给定一个整数数组（有正数有负数），找出总和最大的连续数列，并返回总和。
+
+示例：
+
+输入： [-2,1,-3,4,-1,2,1,-5,4]
+输出： 6
+解释： 连续子数组 [4,-1,2,1] 的和最大，为 6。
+进阶：
+
+如果你已经实现复杂度为 O(n) 的解法，尝试使用更为精妙的分治法求解。
+'''
+
+class Solution(object):
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if not nums:
+            return None
+        dp = [_ for _ in range(len(nums))]
+        dp[0] = nums[0]
+        for i in range(len(nums)-1):
+            dp[i + 1] = max(nums[i + 1], dp[i] + nums[i + 1])
+        return max(dp)
