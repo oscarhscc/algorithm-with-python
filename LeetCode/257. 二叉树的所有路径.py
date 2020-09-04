9e1c66bcc6b459cd61ec890d1d8e7658bc30faff
Oscar Chen <cchen237-c@my.cityu.edu.hk>
2020-09-04 09:41:27 +0800

    Add files via upload

---
+++ LeetCode/257. 二叉树的所有路径.py
@@ -0,0 +1,38 @@
+'''
+给定一个二叉树，返回所有从根节点到叶子节点的路径。
+
+说明: 叶子节点是指没有子节点的节点。
+
+示例:
+
+输入:
+
+   1
+ /   \
+2     3
+ \
+  5
+
+输出: ["1->2->5", "1->3"]
+
+解释: 所有根节点到叶子节点的路径为: 1->2->5, 1->3
+'''
+
+class Solution:
+    def binaryTreePaths(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[str]
+        """
+        def construct_paths(root, path):
+            if root:
+                path += str(root.val)
+                if not root.left and not root.right:  # 当前节点是叶子节点
+                    paths.append(path)  # 把路径加入到答案中
+                else:
+                    path += '->'  # 当前节点不是叶子节点，继续递归遍历
+                    construct_paths(root.left, path)
+                    construct_paths(root.right, path)
+        paths = []
+        construct_paths(root, '')
+        return paths
