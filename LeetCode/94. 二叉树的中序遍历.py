063011c73849325c2039735a7868b405cadc497a
Oscar Chen <cchen237-c@my.cityu.edu.hk>
2020-09-14 09:18:02 +0800

    Add files via upload

---
+++ LeetCode/94. 二叉树的中序遍历.py
@@ -0,0 +1,37 @@
+'''
+给定一个二叉树，返回它的中序 遍历。
+
+示例:
+
+输入: [1,null,2,3]
+   1
+    \
+     2
+    /
+   3
+
+输出: [1,3,2]
+'''
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def inorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        res = []
+        def inorder(root):
+            if root is None:
+                return
+            inorder(root.left)
+            res.append(root.val)
+            inorder(root.right)
+        inorder(root)
+        return res
