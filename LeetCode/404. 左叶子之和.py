2845a1cdf78264da3d930bbdc1a429426411cd43
Oscar Chen <cchen237-c@my.cityu.edu.hk>
2020-09-19 11:30:24 +0800

    Add files via upload

---
+++ LeetCode/404. 左叶子之和.py
@@ -0,0 +1,33 @@
+'''
+计算给定二叉树的所有左叶子之和。
+
+示例：
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+
+在这个二叉树中，有两个左叶子，分别是 9 和 15，所以返回 24
+'''
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def sumOfLeftLeaves(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if root==None:
+            return 0
+        if root.left and root.left.left==None and root.left.right==None:
+            return root.left.val+self.sumOfLeftLeaves(root.right)
+        else:
+            return self.sumOfLeftLeaves(root.left)+self.sumOfLeftLeaves(root.right)
