a65fbd451d6550af9095f8c11d2652f8d342f575
Oscar Chen <cchen237-c@my.cityu.edu.hk>
2020-10-15 10:49:15 +0800

    Add files via upload

---
+++ LeetCode/116. 填充每个节点的下一个右侧节点指针.py
@@ -0,0 +1,40 @@
+'''
+给定一个完美二叉树，其所有叶子节点都在同一层，每个父节点都有两个子节点。二叉树定义如下：
+
+struct Node {
+  int val;
+  Node *left;
+  Node *right;
+  Node *next;
+}
+填充它的每个 next 指针，让这个指针指向其下一个右侧节点。如果找不到下一个右侧节点，则将 next 指针设置为 NULL。
+
+初始状态下，所有 next 指针都被设置为 NULL。
+'''
+
+
+"""
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val=0, left=None, right=None, next=None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+class Solution(object):
+    def connect(self, root):
+        """
+        :type root: Node
+        :rtype: Node
+        """
+        queue = [root]
+        while queue:
+            n = len(queue)
+            for i in range(n):
+                node = queue.pop(0)
+                if node is None:
+                    continue
+                node.next = queue[0] if n != i+1 else None
+                queue += [node.left, node.right]
+        return root
