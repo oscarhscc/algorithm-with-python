b47203333db8f6c42184bda111f76d277abfb4cc
Oscar Chen <cchen237-c@my.cityu.edu.hk>
2020-10-20 09:45:10 +0800

    Add files via upload

---
+++ LeetCode/143. 重排链表.py
@@ -0,0 +1,43 @@
+'''
+给定一个单链表 L：L0→L1→…→Ln-1→Ln ，
+将其重新排列后变为： L0→Ln→L1→Ln-1→L2→Ln-2→…
+
+你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
+
+示例 1:
+
+给定链表 1->2->3->4, 重新排列为 1->4->2->3.
+示例 2:
+
+给定链表 1->2->3->4->5, 重新排列为 1->5->2->4->3.
+'''
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def reorderList(self, head):
+        """
+        :type head: ListNode
+        :rtype: None Do not return anything, modify head in-place instead.
+        """
+        if not head:
+            return
+        # 将链表装载到列表中，以便索引
+        vec = []
+        node = head
+        while node:
+            vec.append(node)
+            node = node.next
+            
+        i, j = 0, len(vec) - 1
+        while i < j:
+            vec[i].next = vec[j]
+            i += 1
+            if i == j:
+                break
+            vec[j].next = vec[i]
+            j -= 1
+        vec[i].next = None
