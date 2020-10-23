e443a38d53fecadde0d3fbf5d14985aac78ec345
Oscar Chen <cchen237-c@my.cityu.edu.hk>
2020-10-23 08:50:27 +0800

    Add files via upload

---
+++ LeetCode/234. 回文链表.py
@@ -0,0 +1,32 @@
+'''
+请判断一个链表是否为回文链表。
+
+示例 1:
+
+输入: 1->2
+输出: false
+示例 2:
+
+输入: 1->2->2->1
+输出: true
+进阶：
+你能否用 O(n) 时间复杂度和 O(1) 空间复杂度解决此题？
+'''
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def isPalindrome(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        res = []
+        while head:
+            res.append(head.val)
+            head = head.next
+        return res == res[::-1]
