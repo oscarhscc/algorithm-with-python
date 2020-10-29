c25d81969462b2e8d12356452d2dd8098567d371
Oscar Chen <cchen237-c@my.cityu.edu.hk>
2020-10-29 10:11:59 +0800

    Add files via upload

---
+++ LeetCode/129. 求根到叶子节点数字之和.py
@@ -0,0 +1,67 @@
+'''
+给定一个二叉树，它的每个结点都存放一个 0-9 的数字，每条从根到叶子节点的路径都代表一个数字。
+
+例如，从根到叶子节点路径 1->2->3 代表数字 123。
+
+计算从根到叶子节点生成的所有数字之和。
+
+说明: 叶子节点是指没有子节点的节点。
+
+示例 1:
+
+输入: [1,2,3]
+    1
+   / \
+  2   3
+输出: 25
+解释:
+从根到叶子节点路径 1->2 代表数字 12.
+从根到叶子节点路径 1->3 代表数字 13.
+因此，数字总和 = 12 + 13 = 25.
+示例 2:
+
+输入: [4,9,0,5,1]
+    4
+   / \
+  9   0
+ / \
+5   1
+输出: 1026
+解释:
+从根到叶子节点路径 4->9->5 代表数字 495.
+从根到叶子节点路径 4->9->1 代表数字 491.
+从根到叶子节点路径 4->0 代表数字 40.
+因此，数字总和 = 495 + 491 + 40 = 1026.
+'''
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def sumNumbers(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        def helper(root):
+            if not root:
+                return []
+            if not root.left and not root.right:
+                return [[root.val]]
+            
+            lpath = [[root.val] + path for path in helper(root.left)]
+            rpath = [[root.val] + path for path in helper(root.right)]
+            return lpath + rpath
+        
+        paths = helper(root)
+        res = 0
+        for path in paths:
+            tmp = 0
+            for val in path:
+                tmp = tmp*10 + val
+            res += tmp
+        return res
